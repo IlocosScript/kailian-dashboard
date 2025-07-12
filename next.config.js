770cef3f6c961ca1c2e41a7c3f569c3b6aac266b
ilocosscriptadmin <ilocosscript@gmail.com>
2025-07-12 20:41:04 +0900

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    appDir: true,
+  },
 };
 
 module.exports = nextConfig;
